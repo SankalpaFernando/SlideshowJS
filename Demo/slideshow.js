3816ddb7c09864300c7a12e126f34f7ee86a5904
Sankalpa Fernando <sankalpafernando2017@gmail.com>
2019-10-06 11:22:43 +0530

    Initial commit

---
+++ Demo/slideshow.js
@@ -0,0 +1,33 @@
+//Global Scope
+var Index = 0;
+
+var cssId="slidecss";
+if(!document.getElementById(cssId)){
+    var head =document.head;
+    var link =document.createElement("link");
+    link.id=cssId;
+    link.href="slideshow.css";
+    link.rel="stylesheet";
+    head.appendChild(link);
+}
+if(!document.getElementById("animatecss")){
+  var head =document.head;
+  var link =document.createElement("link");
+  link.id="animatecss";
+  link.href="animate.css";
+  link.rel="stylesheet";
+  head.appendChild(link);
+}
+sliding();
+function sliding() {
+  let slides = Array.from(document.querySelectorAll(".slide"));
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  Index++;
+  if (Index > slides.length) {
+    Index = 1;
+  }
+  slides[Index - 1].style.display = "block";
+  setTimeout(sliding, 2000);
+}
